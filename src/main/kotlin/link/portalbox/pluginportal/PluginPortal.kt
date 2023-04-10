17abfeeb655877b80944978330fc4ad3b83b0807
Dawson <88049488+Dawsson@users.noreply.github.com>
2023-04-09 17:26:31 -0700

    Removed listener

--- src/main/kotlin/link/portalbox/pluginportal/PluginPortal.kt
+++ src/main/kotlin/link/portalbox/pluginportal/PluginPortal.kt
@@ -4,13 +4,11 @@ import link.portalbox.pluginportal.command.PPCommand
 import link.portalbox.pluginportal.file.Config
 import link.portalbox.pluginportal.file.Data
 import link.portalbox.pluginportal.listener.PluginValidator
-import link.portalbox.pluginportal.listener.UpdateListener
 import link.portalbox.pluginportal.util.*
 import link.portalbox.pplib.manager.MarketplacePluginManager
 import link.portalbox.pplib.manager.MarketplacePluginManager.loadIndex
 import link.portalbox.pplib.service.SpigotMCService
 import link.portalbox.pplib.type.MarketplaceService
-import link.portalbox.pplib.util.getLatestPPVersion
 import link.portalbox.pplib.util.getPPVersions
 import org.bstats.bukkit.Metrics
 import org.bukkit.plugin.java.JavaPlugin
@@ -44,7 +42,6 @@ class PluginPortal : JavaPlugin() {
             getCommand("pluginportal")!!.tabCompleter = command
 
             server.pluginManager.registerEvents(PluginValidator(), this)
-            server.pluginManager.registerEvents(UpdateListener(this), this)
 
             setupMetrics(Metrics(this, 18005))
             deleteOutdatedPP(this)
