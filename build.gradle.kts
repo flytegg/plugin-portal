32ffac9d648181bbb384c6a5f886cc542cfe6bf9
Nuckerr <nuckermail@gmail.com>
2023-04-25 17:34:14 +0100

    Add hangar publish task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
+import io.papermc.hangarpublishplugin.model.Platforms
+import java.io.ByteArrayOutputStream
+
 plugins {
     kotlin("jvm") version "1.7.21"
     id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("io.papermc.hangar-publish-plugin") version "0.0.5"
     id("xyz.jpenilla.run-paper") version "2.0.1"
 }
 
@@ -22,6 +26,33 @@ dependencies {
     compileOnly("com.google.code.gson:gson:2.10.1")
 }
 
+hangarPublish {
+    val owner = "stephen"
+    val slug = "PluginPortal"
+    val versions: List<String> = listOf("1.8-1.19.4")
+
+    publications.register("Release") {
+        if(project.properties["hangar-publish-plugin.use-dev-endpoint"] as String == "true") {
+            apiEndpoint.set("https://hangar.papermc.dev/api/v1/")
+        }
+
+        namespace(owner, slug)
+        version.set(project.version as String)
+        channel.set(if(version.get().endsWith("-SNAPSHOT")) "prerelease" else "release")
+
+        val commitLog = getCommitHistory(project.properties["release-start-commit"] as String)
+
+        changelog.set("# Release ${project.version}\n ${commitLog.joinToString(separator = "") { formatCommitLog(it) }} \n")
+
+        platforms {
+            register(Platforms.PAPER) {
+                jar.set(tasks.jar.flatMap { it.archiveFile })
+                platformVersions.set(versions)
+            }
+        }
+    }
+}
+
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
@@ -41,4 +72,24 @@ tasks {
     runServer {
         minecraftVersion("1.19.4")
     }
+}
+
+fun getCommitHistory(startHash: String, endHash: String = "HEAD"): List<String> {
+    val output: String = ByteArrayOutputStream().use { outputStream ->
+        project.exec {
+            executable("git")
+            args("log",  "$startHash..$endHash", "--format=format:%h %s")
+            standardOutput = outputStream
+        }
+        outputStream.toString()
+    }
+    return output.split("\n")
+}
+
+fun formatCommitLog(commitLog: String): String {
+    // Assuming log is in the format: 2059265 Commit message here
+    //println("fixing $commitLog")
+    val hash = commitLog.take(7)
+    val message = commitLog.substring(8) // Get message after commit hash + space between
+    return "* [$hash](https://github.com/Nuckerr/plugin-portal/commit/$hash) $message\n"
 }
