26ec3c21f3b7db1a87e5e980a45d0ac6ece4accf
Dawson <hello@dawson.gg>
2023-08-31 12:16:41 -0400

    janky request plugin added

--- cli/src/main/kotlin/gg/flyte/pluginPortal/commands/plugins/RequestPluginCommand.kt
+++ cli/src/main/kotlin/gg/flyte/pluginPortal/commands/plugins/RequestPluginCommand.kt
@@ -1,15 +1,22 @@
 package gg.flyte.pluginPortal.commands.plugins
 
+import com.github.ajalt.mordant.table.table
+import gg.flyte.common.api.API
 import gg.flyte.common.api.dataClasses.MarketplacePlugin
 import gg.flyte.pluginPortal.commands.abstractClasses.PluginAPICommand
+import gg.flyte.pluginPortal.type.config.Config
+import gg.flyte.pluginPortal.type.server.ServerManager
 
 class RequestPluginCommand : PluginAPICommand(
     name = "request",
     help = "Request a Plugin to be supported."
 ) {
     override fun finishCommand(plugin: MarketplacePlugin) {
-        println(plugin.displayInfo.name + " - has been chosen")
+
+        Config.terminal.println(table {
+            body { row("${plugin.displayInfo.name} has been requested to be updated.") }
+        })
+
+        API.requestPluginById(plugin.id, ServerManager.getActiveServer()?.softwareType?.primarySupportedPlatformType ?: return)
     }
-
-
 }
