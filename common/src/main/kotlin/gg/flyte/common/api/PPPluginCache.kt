4d84909391cd900c6beb6eb9131601be43e63c10
Dawson <hello@dawson.gg>
2023-09-02 09:46:44 -0400

    caching done????

--- common/src/main/kotlin/gg/flyte/common/api/PPPluginCache.kt
+++ common/src/main/kotlin/gg/flyte/common/api/PPPluginCache.kt
@@ -4,37 +4,56 @@ import com.github.benmanes.caffeine.cache.Cache
 import com.github.benmanes.caffeine.cache.Caffeine
 import gg.flyte.common.api.dataClasses.MarketplacePlugin
 import gg.flyte.common.type.api.service.PlatformType
-import java.security.Key
 import java.util.concurrent.TimeUnit
-import javax.lang.model.element.Name
 
 object PPPluginCache {
 
     /**
-     * Key: Plugin Name
-     * Value: MarketplacePlugin
+     * Key: Search Term
+     * Value: List<MarketplacePlugin>
      */
-    val pluginCache: Cache<String, MarketplacePlugin> = Caffeine.newBuilder()
+    private val pluginCache: Cache<String, List<MarketplacePlugin>> = Caffeine.newBuilder()
         .expireAfterWrite(5, TimeUnit.MINUTES)
         .build()
 
-    fun getPluginByName(name: String): MarketplacePlugin? {
-        return pluginCache.getIfPresent(name)
-    }
-
-    fun getPluginById(id: String): MarketplacePlugin? {
-        return pluginCache.asMap().values.find { it.id == id }
-    }
-
-    fun searchForPluginsByName(name: String, platformType: PlatformType): List<MarketplacePlugin> {
-        API.searchForPluginsByName(name, platformType.name).body()?.result?.let { list ->
-            list.forEach {
-                pluginCache.put(it.displayInfo.name, it)
+    fun getPluginsByName(name: String) = arrayListOf<MarketplacePlugin>().apply {
+        pluginCache.asMap().keys.forEach {
+            if (name.startsWith(it, true)) {
+                addAll(pluginCache.asMap()[it]!!)
             }
         }
 
-        return pluginCache.asMap()
-            .filter { it.key.startsWith(name) || it.key.contains(name) }
-            .map { it.value }
+        filter { it.displayInfo.name.startsWith(name, true) }
+    }
+
+
+    fun getPluginById(id: String): MarketplacePlugin? {
+        pluginCache.asMap().values.forEach {
+            it.forEach { plugin ->
+                if (plugin.id == id) return plugin
+            }
+        }
+
+        return null
+    }
+
+    fun searchForPluginsByName(name: String, platformType: PlatformType): List<MarketplacePlugin> {
+
+        val cachedMatches = mutableListOf<MarketplacePlugin>().apply {
+            addAll(getPluginsByName(name))
+
+            if (size >= 25 || pluginCache.asMap().containsKey(name)) return this
+            else {
+                API.searchForPluginsByName(name, platformType.name).body()?.result?.let { list ->
+                    pluginCache.put(name, list)
+
+                    return list.filter {
+                        it.displayInfo.name.startsWith(name) || it.displayInfo.name.contains(name)
+                    }
+                }
+            }
+        }
+
+        return cachedMatches
     }
 }
