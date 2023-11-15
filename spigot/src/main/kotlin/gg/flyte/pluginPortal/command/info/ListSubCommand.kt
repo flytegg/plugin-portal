0991b3bf74ea9df5dedb2d6afef52e086ed7545e
Dawson <hello@dawson.gg>
2023-11-14 19:25:36 -0500

    feat(list): Better looking /list command

--- spigot/src/main/kotlin/gg/flyte/pluginPortal/command/info/ListSubCommand.kt
+++ spigot/src/main/kotlin/gg/flyte/pluginPortal/command/info/ListSubCommand.kt
@@ -1,8 +1,11 @@
 package gg.flyte.pluginPortal.command.info
 
+import gg.flyte.pluginPortal.type.extension.sendInfo
 import gg.flyte.pluginPortal.type.manager.PPPluginCache
 import gg.flyte.pluginPortal.type.manager.language.Message.toComponent
+import gg.flyte.twilight.extension.solidLine
 import net.kyori.adventure.audience.Audience
+import net.kyori.adventure.text.Component
 import revxrsal.commands.annotation.Command
 import revxrsal.commands.annotation.Subcommand
 import revxrsal.commands.bukkit.annotation.CommandPermission
@@ -13,8 +16,15 @@ class ListSubCommand {
     @Subcommand("list", "ls")
     @CommandPermission("pluginportal.list")
     fun listSubCommand(sender: Audience) {
-        PPPluginCache.getInstalledPlugins().forEach {
-            sender.sendMessage(it.name.toComponent())
+        val strike = Component.text("").solidLine()
+        with(sender) {
+            sendMessage(strike)
+            if (PPPluginCache.getInstalledPlugins().isEmpty()) { sendInfo("No plugins installed.") }
+
+            PPPluginCache.getInstalledPlugins().forEach {
+                sendMessage(" - ${it.getUniqueName()} | ${it.version ?: "Unknown Version"}".toComponent())
+            }
+            sendMessage(strike)
         }
     }
 
