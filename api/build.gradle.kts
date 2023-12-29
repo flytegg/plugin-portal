b59da7eb4ea6aee06259e71738a5935bb68bb91c
Dawson <hello@dawson.gg>
2023-12-29 15:46:18 -0500

    compileOnly of gson

---
+++ api/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("net.kyori.blossom")
+    id("org.jetbrains.gradle.plugin.idea-ext")
+}
+
+sourceSets {
+    main {
+        blossom {
+            javaSources {
+                property("version", project.version.toString())
+                property("impl_version", "git-PluginPortal-${project.version}:${rootProject.latestCommitHash()}")
+            }
+        }
+    }
+}
+
+dependencies {
+    compileOnlyApi(libs.gson)
+
+    compileOnlyApi(libs.snakeYaml)
+    compileOnlyApi(libs.guava)
+}
+
+java {
+    withJavadocJar()
+}
+
+publishShadowJar()
