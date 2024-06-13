8d9ca484494225c00e056d92522aef42a838e5da
MLGPenguin <baww123@gmail.com>
2024-06-13 22:41:36 +1000

    Jar Size: 7.51mb -> 3.9mb

--- plugin/src/main/kotlin/gg/flyte/pluginportal/plugin/PluginPortal.kt
+++ plugin/src/main/kotlin/gg/flyte/pluginportal/plugin/PluginPortal.kt
@@ -1,12 +1,8 @@
 package gg.flyte.pluginportal.plugin
 
-import com.google.gson.GsonBuilder
 import gg.flyte.pluginportal.plugin.command.CommandManager
-import gg.flyte.pluginportal.plugin.command.HelpSubCommand
-import gg.flyte.pluginportal.plugin.command.ViewSubCommand
 import io.papermc.lib.PaperLib
 import org.bukkit.plugin.java.JavaPlugin
-import revxrsal.commands.bukkit.BukkitCommandHandler
 
 class PluginPortal : JavaPlugin() {
 
