6c8aff08b7917a487fd19cb03df91513a5277098
MLGPenguin <baww123@gmail.com>
2024-06-21 01:18:03 +1000

    Fix Desc. & Add hover to view full length

--- plugin/src/main/kotlin/gg/flyte/pluginportal/plugin/command/ListSubCommand.kt
+++ plugin/src/main/kotlin/gg/flyte/pluginportal/plugin/command/ListSubCommand.kt
@@ -1,5 +1,6 @@
 package gg.flyte.pluginportal.plugin.command
 
+import gg.flyte.pluginportal.plugin.chat.*
 import gg.flyte.pluginportal.plugin.manager.LocalPluginCache
 import gg.flyte.pluginportal.plugin.util.*
 import net.kyori.adventure.audience.Audience
