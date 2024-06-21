950b9d1aebb8c78977134bb217ca5425f8d7fd8a
Dawson <hello@dawson.gg>
2024-06-21 11:01:24 -0400

    Folia "support"

--- plugin/src/main/kotlin/gg/flyte/pluginportal/plugin/util/Schedulers.kt
+++ plugin/src/main/kotlin/gg/flyte/pluginportal/plugin/util/Schedulers.kt
@@ -3,8 +3,22 @@ package gg.flyte.pluginportal.plugin.util
 import gg.flyte.pluginportal.plugin.PluginPortal
 import org.bukkit.Bukkit
 
-fun async(block: () -> Unit) =
-    Bukkit.getScheduler().runTaskAsynchronously(PluginPortal.instance, block)
+fun isFolia(): Boolean {
+    try {
+        Class.forName("io.papermc.paper.threadedregions.RegionizedServer")
+        return true
+    } catch (e: ClassNotFoundException) {
+        return false
+    }
+}
+
+fun async(block: () -> Unit) {
+    if (isFolia()) {
+        block()
+    } else {
+        Bukkit.getScheduler().runTaskAsynchronously(PluginPortal.instance, block)
+    }
+}
 
 fun delay(ticks: Long, block: () -> Unit) =
     Bukkit.getScheduler().runTaskLater(PluginPortal.instance, block, ticks)
