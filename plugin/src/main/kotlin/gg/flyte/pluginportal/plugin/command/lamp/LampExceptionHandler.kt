e3a011dfcb06820e45b8e1a7dcc6ca6f5c4073c5
MLGPenguin <baww123@gmail.com>
2024-06-29 14:10:43 +1000

    Fix build issue from previous commit

--- plugin/src/main/kotlin/gg/flyte/pluginportal/plugin/command/lamp/LampExceptionHandler.kt
+++ plugin/src/main/kotlin/gg/flyte/pluginportal/plugin/command/lamp/LampExceptionHandler.kt
@@ -1,10 +1,7 @@
 package gg.flyte.pluginportal.plugin.command.lamp
 
 import gg.flyte.pluginportal.common.types.MarketplacePlatform
-import gg.flyte.pluginportal.plugin.chat.Status
-import gg.flyte.pluginportal.plugin.chat.boxed
-import gg.flyte.pluginportal.plugin.chat.sendFailureMessage
-import gg.flyte.pluginportal.plugin.chat.status
+import gg.flyte.pluginportal.plugin.chat.*
 import net.kyori.adventure.platform.bukkit.BukkitAudiences
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor
@@ -24,10 +21,7 @@ class LampExceptionHandler(private val audiences: BukkitAudiences): BukkitExcept
         var comp = status(Status.FAILURE, "Invalid ${exception.parameter.name}: ${exception.input}")
 
         if (MarketplacePlatform::class.java == exception.parameter.type)
-            comp = comp
-                .appendNewline()
-                .appendNewline()
-                .append(text("- Acceptable values are: ${MarketplacePlatform.entries.joinToString()}", NamedTextColor.GRAY))
+            comp = comp.appendSecondary("\n\n- Acceptable values are: ${MarketplacePlatform.entries.joinToString()}")
 
         audiences.sender(actor.sender).sendMessage(comp.boxed())
     }
