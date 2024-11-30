74e989b7bcc95e03cde4777f4a417659ca528065
MLGPenguin <baww123@gmail.com>
2024-11-30 20:45:08 +1000

    WIP exception handling

--- plugin/src/main/kotlin/gg/flyte/pluginportal/plugin/command/lamp/LampExceptionHandler.kt
+++ plugin/src/main/kotlin/gg/flyte/pluginportal/plugin/command/lamp/LampExceptionHandler.kt
@@ -1,28 +1,38 @@
 package gg.flyte.pluginportal.plugin.command.lamp
 
-import gg.flyte.pluginportal.common.types.MarketplacePlatform
-import gg.flyte.pluginportal.plugin.chat.*
+import gg.flyte.pluginportal.plugin.chat.sendFailureMessage
 import net.kyori.adventure.platform.bukkit.BukkitAudiences
-import net.kyori.adventure.text.Component.text
-import net.kyori.adventure.text.format.NamedTextColor
-import revxrsal.commands.bukkit.exception.BukkitExceptionAdapter
-import revxrsal.commands.bukkit.sender
-import revxrsal.commands.command.CommandActor
+import revxrsal.commands.bukkit.actor.BukkitCommandActor
+import revxrsal.commands.bukkit.exception.BukkitExceptionHandler
 import revxrsal.commands.exception.EnumNotFoundException
 import revxrsal.commands.exception.MissingArgumentException
+import revxrsal.commands.node.ParameterNode
 
-class LampExceptionHandler(private val audiences: BukkitAudiences): BukkitExceptionAdapter() {
+class LampExceptionHandler(private val audiences: BukkitAudiences): BukkitExceptionHandler() {
 
-    override fun missingArgument(actor: CommandActor, exception: MissingArgumentException) {
-        sendFailureMessage(audiences.sender(actor.sender), "No value provided for ${exception.parameter.name}")
+    private val BukkitCommandActor.audience get() = audiences.sender(sender())
+
+    override fun onMissingArgument(ex: MissingArgumentException, actor: BukkitCommandActor, parameter: ParameterNode<BukkitCommandActor, *>) {
+        // TODO: Test
+        sendFailureMessage(actor.audience, "No value provided for ${parameter.name()}")
     }
 
-    override fun invalidEnumValue(actor: CommandActor, exception: EnumNotFoundException) {
+    override fun onEnumNotFound(ex: EnumNotFoundException, actor: BukkitCommandActor) {
+        // TODO: Parameter name not available in v4
+//        var comp = status(Status.FAILURE, "Invalid ${ex.}: ${ex.input()}")
+//
+//        if (MarketplacePlatform::class.java == exception.parameter.type)
+//            comp = comp.appendSecondary("\n\n- Acceptable values are: ${MarketplacePlatform.entries.joinToString()}")
+//
+//        audiences.sender(actor.sender).sendMessage(comp.boxed())
+    }
+
+/*    override fun invalidEnumValue(actor: CommandActor, exception: EnumNotFoundException) {
         var comp = status(Status.FAILURE, "Invalid ${exception.parameter.name}: ${exception.input}")
 
         if (MarketplacePlatform::class.java == exception.parameter.type)
             comp = comp.appendSecondary("\n\n- Acceptable values are: ${MarketplacePlatform.entries.joinToString()}")
 
         audiences.sender(actor.sender).sendMessage(comp.boxed())
-    }
+    }*/
 }
