424487d50ee3a7369765e65bbddc7eacabd81434
MLGPenguin <baww123@gmail.com>
2024-11-30 20:44:46 +1000

    Move to new v4 creation logic

--- plugin/src/main/kotlin/gg/flyte/pluginportal/plugin/command/CommandManager.kt
+++ plugin/src/main/kotlin/gg/flyte/pluginportal/plugin/command/CommandManager.kt
@@ -1,33 +1,36 @@
 package gg.flyte.pluginportal.plugin.command
 
-import gg.flyte.pluginportal.common.SearchPlugins
-import gg.flyte.pluginportal.common.types.LocalPlugin
-import gg.flyte.pluginportal.common.types.Plugin
 import gg.flyte.pluginportal.plugin.PluginPortal.Companion.instance
 import gg.flyte.pluginportal.plugin.command.lamp.LampExceptionHandler
-import gg.flyte.pluginportal.plugin.manager.LocalPluginCache
-import gg.flyte.pluginportal.plugin.util.async
 import net.kyori.adventure.platform.bukkit.BukkitAudiences
-import revxrsal.commands.bukkit.BukkitCommandHandler
-import java.io.File
+import revxrsal.commands.Lamp
+import revxrsal.commands.bukkit.BukkitLamp
+import revxrsal.commands.bukkit.actor.BukkitCommandActor
 
 object CommandManager {
 
     init {
-        BukkitCommandHandler.create(instance).apply {
+        val audiences = BukkitAudiences.create(instance)
+
+        val lamp: Lamp<BukkitCommandActor> = BukkitLamp
+            .builder(instance)
+            .exceptionHandler(LampExceptionHandler(audiences))
+            .build()
+            .apply {
+                registerCommands()
+            }
+
+/*        BukkitCommandHandler.create(instance).apply {
             flagPrefix = "--"
 
             val audiences = BukkitAudiences.create(instance)
 
             enableAdventure(audiences)
-            registerAutoComplete()
-            registerCommands()
             registerBrigadier()
-            exceptionHandler = LampExceptionHandler(audiences)
-        }
+        }*/
     }
 
-    private fun BukkitCommandHandler.registerCommands() {
+    private fun Lamp<BukkitCommandActor>.registerCommands() {
         register(
             InstallSubCommand(),
             UpdateSubCommand(),
@@ -40,36 +43,5 @@ object CommandManager {
         )
     }
 
-    private fun BukkitCommandHandler.registerAutoComplete() {
-        autoCompleter
-            .registerSuggestion("marketplacePluginSearch") { args, _, _ ->
-                val searchName = args.last()
 
-                if (searchName.length == 2) async { SearchPlugins.search(searchName) }
-
-                if (searchName.length <= 2)
-                    listOf("Keep typing...")
-                else {
-                    SearchPlugins.getCachedSearch(searchName)
-                        ?.map(Plugin::name)
-                        ?.filter { it.startsWith(searchName, true) }
-                        ?.map { "\"$it\"" }
-                        ?: listOf("Loading...")
-                }
-
-            }
-            .registerSuggestion("installedPluginSearch") { args, _, _ ->
-                LocalPluginCache.map(LocalPlugin::name)
-            }
-            .registerSuggestion("installedPluginSearchWithoutSelf") { args, _, _ ->
-                LocalPluginCache.map(LocalPlugin::name)
-                    .filter { name -> name != instance.description.name }
-            }
-            .registerSuggestion("pluginFileSearch") { args, _, _ ->
-                File("plugins").listFiles()!!
-                    .filter { file -> file.isFile }
-                    .filter { file -> file.name.endsWith(".jar") }
-                    .map { file -> file.name }
-            }
-    }
 }
